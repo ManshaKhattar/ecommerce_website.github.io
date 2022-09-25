912829b80385c4f3256d4677051aebbcc5592181
Mansha Khattar <manshakhattar@gmail.com>
2022-09-25 13:37:58 +0530

    : Adding settings file

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4o_80@7ek+qu5l_1fuci5qi+*t+7ov_989x*2b4%4a=%y3jira'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
