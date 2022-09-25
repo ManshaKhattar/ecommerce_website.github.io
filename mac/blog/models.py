0a3b6b90a8813099f4c436eb4f4588073dc23665
Mansha Khattar <manshakhattar@gmail.com>
2022-09-25 13:10:59 +0530

    Code for e-commerce application

---
+++ mac/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key= True)
+    title = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500, default="")
+    chead0 = models.CharField(max_length=5000, default="")
+    head1 = models.CharField(max_length=500, default="")
+    chead1 = models.CharField(max_length=5000, default="")
+    head2 = models.CharField(max_length=500, default="")
+    chead2 = models.CharField(max_length=5000, default="")
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='shop/images', default="")
+
+    def __str__(self):
+        return self.title
+
+class Orders(models.Model):
+    order_id= models.AutoField(primary_key=True)
+    items_json= models.CharField(max_length=5000)
+    amount=models.IntegerField(default=0)
+    name=models.CharField(max_length=90)
+    email=models.CharField(max_length=111)
+    address=models.CharField(max_length=111)
+    city=models.CharField(max_length=111)
+    state=models.CharField(max_length=111)
+    zip_code=models.CharField(max_length=111)
+    phone=models.CharField(max_length=111, default="")
